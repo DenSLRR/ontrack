33f61baee21378d67747263dc2600b844ae9c3d9
Denis <denslrrwork@gmail.com>
2024-06-28 11:51:34 +0300

    defien emit

--- src/App.vue
+++ src/App.vue
@@ -2,59 +2,40 @@
     <TheHeader />
 
     <main class="flex flex-grow flex-col">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, aut,
-        earum quidem, magni repellendus laborum eos voluptatum corrupti sit
-        libero hic repellat dolorum ipsa nihil deserunt iste? Obcaecati numquam
-        culpa facere id et odit doloremque suscipit magnam recusandae? Quaerat,
-        in, fuga ea ipsam dolor repellendus obcaecati aliquid odit unde aperiam
-        aut! Quam doloribus impedit, sit blanditiis quas nobis molestias maxime
-        itaque inventore similique! Dolorum quas earum beatae quasi
-        necessitatibus quia asperiores facilis eligendi quam nostrum autem
-        cupiditate similique neque iure et eveniet explicabo porro fuga nam
-        quibusdam veniam soluta, mollitia odit! Accusantium id praesentium
-        possimus doloremque illum ipsum, deserunt quaerat? Fugiat at quis earum
-        in alias consequuntur adipisci voluptates voluptatibus ullam repellendus
-        perspiciatis totam cupiditate ad recusandae, molestiae ipsam quos animi
-        corrupti vel sequi delectus? Alias, quod amet quos, dignissimos, dolore
-        corrupti ipsam veniam quisquam ipsa ab provident eum aspernatur soluta
-        voluptatem error omnis quis nemo fugiat autem illo odio sed? Cumque
-        saepe quas facere, recusandae, voluptatem, libero quis doloribus quae
-        explicabo perspiciatis veniam deleniti reiciendis id cum. Iusto animi
-        praesentium deserunt perspiciatis. Facilis esse a quidem mollitia quam
-        laborum, repellat laboriosam tempore possimus consequuntur molestias
-        nemo ipsa ut commodi voluptates. Veniam illum, corporis tenetur
-        doloribus rerum nulla saepe autem pariatur voluptatum blanditiis quis
-        earum maiores voluptas aspernatur totam quas sit perspiciatis sunt
-        expedita ullam incidunt mollitia natus odit tempora? Quos quasi eaque
-        iusto voluptas inventore voluptates commodi architecto itaque ex
-        necessitatibus incidunt, optio repellendus ipsam amet. Quod quidem
-        quasi, inventore beatae dignissimos, nulla commodi qui eligendi vero
-        fugiat in rem, voluptatibus dolore unde harum ipsum veniam blanditiis
-        saepe dolorum deserunt iure tenetur vel. Fugit, quae ratione. A neque
-        odit vitae nostrum laborum totam odio, dolorem suscipit, hic facilis
-        dolores explicabo quas porro esse inventore tempora pariatur veniam
-        quos, id deleniti obcaecati debitis quam. Temporibus dicta, asperiores
-        sed pariatur est rem, odit qui sapiente quos fuga laudantium hic quas
-        tempora? Voluptatibus facilis nulla, doloremque esse reiciendis
-        exercitationem eos architecto quibusdam illo sunt molestiae ratione
-        laboriosam omnis possimus animi eaque assumenda id ullam nobis quasi
-        minima ea iure eius? Amet blanditiis autem, nisi voluptas harum
-        laboriosam repudiandae atque molestias cum non perferendis, neque
-        suscipit mollitia temporibus commodi assumenda vel expedita! Odit
-        accusamus doloribus, magnam quo saepe atque in! Accusantium dignissimos
-        odit autem impedit officiis vitae dolor aut unde libero, eos suscipit
-        magni iure quaerat quo mollitia. Nobis molestias odit provident deserunt
-        ipsam qui debitis doloribus tempora unde! Totam laboriosam odio eos vel
-        itaque labore quo minus ex vero ab modi quibusdam voluptatibus iusto
-        harum architecto omnis est, alias
+        <TheTimeline v-show="currentPage === PAGE_TIMELINE" />
+        <TheActivities v-show="currentPage === PAGE_ACTIVITIES" />
+        <TheProgress v-show="currentPage === PAGE_PROGRESS" />
     </main>
 
-    <TheNav />
+    <TheNav :current-page="currentPage" @page-change="onPageChange" />
 </template>
 
 <script setup>
 import TheHeader from "./components/TheHeader.vue";
 import TheNav from "./components/TheNav.vue";
+import TheTimeline from "./pages/TheTimeline.vue";
+import TheActivities from "./pages/TheActivities.vue";
+import TheProgress from "./pages/TheProgress.vue";
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from "./constants.js";
+import { ref, reactive } from "vue";
+
+const currentPage = ref(normalizePageHash());
+
+function normalizePageHash() {
+    const hash = window.location.hash.slice(1);
+
+    if ([PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS].includes(hash)) {
+        return hash;
+    }
+
+    window.location.hash = PAGE_TIMELINE;
+
+    return PAGE_TIMELINE;
+}
+
+const onPageChange = (page) => {
+    currentPage.value = page;
+};
 </script>
 
 <style></style>
--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -8,7 +8,7 @@
                 :class="{
                     'bg-gray-200 pointer-events-none': page === currentPage,
                 }"
-                @click="currentPage = page"
+                @click="() => handleClick(page)"
             >
                 <component :is="icon" class="h-6 w-6" /> {{ page }}
             </NavItem>
@@ -23,15 +23,21 @@ import {
     ChartBarIcon,
 } from "@heroicons/vue/24/outline";
 import NavItem from "./NavItem.vue";
-import { ref } from "vue";
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from "../constants";
+
+const props = defineProps(["currentPage"]);
+
+const emits = defineEmits(["page-change"]);
 
 const navItems = {
-    timeline: ClockIcon,
-    activities: ListBulletIcon,
-    progress: ChartBarIcon,
+    [PAGE_TIMELINE]: ClockIcon,
+    [PAGE_ACTIVITIES]: ListBulletIcon,
+    [PAGE_PROGRESS]: ChartBarIcon,
 };
 
-const currentPage = ref("timeline");
+const handleClick = (page) => {
+    emits("page-change", page);
+};
 </script>
 
 <style scoped></style>

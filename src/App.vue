ff36c76114e98af328edbbd38109d7b92c16b480
Denis <denslrrwork@gmail.com>
2024-06-28 10:42:37 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+    <TheHeader />
+
+    <main class="flex flex-grow flex-col">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, aut,
+        earum quidem, magni repellendus laborum eos voluptatum corrupti sit
+        libero hic repellat dolorum ipsa nihil deserunt iste? Obcaecati numquam
+        culpa facere id et odit doloremque suscipit magnam recusandae? Quaerat,
+        in, fuga ea ipsam dolor repellendus obcaecati aliquid odit unde aperiam
+        aut! Quam doloribus impedit, sit blanditiis quas nobis molestias maxime
+        itaque inventore similique! Dolorum quas earum beatae quasi
+        necessitatibus quia asperiores facilis eligendi quam nostrum autem
+        cupiditate similique neque iure et eveniet explicabo porro fuga nam
+        quibusdam veniam soluta, mollitia odit! Accusantium id praesentium
+        possimus doloremque illum ipsum, deserunt quaerat? Fugiat at quis earum
+        in alias consequuntur adipisci voluptates voluptatibus ullam repellendus
+        perspiciatis totam cupiditate ad recusandae, molestiae ipsam quos animi
+        corrupti vel sequi delectus? Alias, quod amet quos, dignissimos, dolore
+        corrupti ipsam veniam quisquam ipsa ab provident eum aspernatur soluta
+        voluptatem error omnis quis nemo fugiat autem illo odio sed? Cumque
+        saepe quas facere, recusandae, voluptatem, libero quis doloribus quae
+        explicabo perspiciatis veniam deleniti reiciendis id cum. Iusto animi
+        praesentium deserunt perspiciatis. Facilis esse a quidem mollitia quam
+        laborum, repellat laboriosam tempore possimus consequuntur molestias
+        nemo ipsa ut commodi voluptates. Veniam illum, corporis tenetur
+        doloribus rerum nulla saepe autem pariatur voluptatum blanditiis quis
+        earum maiores voluptas aspernatur totam quas sit perspiciatis sunt
+        expedita ullam incidunt mollitia natus odit tempora? Quos quasi eaque
+        iusto voluptas inventore voluptates commodi architecto itaque ex
+        necessitatibus incidunt, optio repellendus ipsam amet. Quod quidem
+        quasi, inventore beatae dignissimos, nulla commodi qui eligendi vero
+        fugiat in rem, voluptatibus dolore unde harum ipsum veniam blanditiis
+        saepe dolorum deserunt iure tenetur vel. Fugit, quae ratione. A neque
+        odit vitae nostrum laborum totam odio, dolorem suscipit, hic facilis
+        dolores explicabo quas porro esse inventore tempora pariatur veniam
+        quos, id deleniti obcaecati debitis quam. Temporibus dicta, asperiores
+        sed pariatur est rem, odit qui sapiente quos fuga laudantium hic quas
+        tempora? Voluptatibus facilis nulla, doloremque esse reiciendis
+        exercitationem eos architecto quibusdam illo sunt molestiae ratione
+        laboriosam omnis possimus animi eaque assumenda id ullam nobis quasi
+        minima ea iure eius? Amet blanditiis autem, nisi voluptas harum
+        laboriosam repudiandae atque molestias cum non perferendis, neque
+        suscipit mollitia temporibus commodi assumenda vel expedita! Odit
+        accusamus doloribus, magnam quo saepe atque in! Accusantium dignissimos
+        odit autem impedit officiis vitae dolor aut unde libero, eos suscipit
+        magni iure quaerat quo mollitia. Nobis molestias odit provident deserunt
+        ipsam qui debitis doloribus tempora unde! Totam laboriosam odio eos vel
+        itaque labore quo minus ex vero ab modi quibusdam voluptatibus iusto
+        harum architecto omnis est, alias
+    </main>
+
+    <TheNav />
+</template>
+
+<script setup>
+import TheHeader from "./components/TheHeader.vue";
+import TheNav from "./components/TheNav.vue";
+</script>
+
+<style></style>
---
+++ src/components/TheLogo.vue
@@ -0,0 +1,13 @@
+<template>
+    <a href="#">
+        <img src="../assets/logo.png" alt="" class="h-9">
+    </a>
+</template>
+
+<script setup>
+    
+</script>
+
+<style  scoped>
+
+</style>
